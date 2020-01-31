1536a949b41d7e008268e6cec839f23fbf8bfb19
FuryOfTheStars <unknown@example.com>
2020-01-31 13:27:11 -0800

    Add options to choose which recipes and techs are reverted.

--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -1,13 +1,3 @@
-local flamethrowerammorecipe = data.raw["recipe"]["flamethrower-ammo"]
-for _, ingredient in pairs (flamethrowerammorecipe.ingredients) do
-	if ingredient.name == "crude-oil" then
-		ingredient.name = "light-oil"
-		ingredient.amount = 50
-		break
-	end
-end
-table.insert(flamethrowerammorecipe.ingredients, {type="fluid", name="heavy-oil", amount=50})
-
 local basicoilprocessingrecipe = data.raw["recipe"]["basic-oil-processing"]
 for _, result in pairs (basicoilprocessingrecipe.results) do
 	if result.name == "petroleum-gas" then
@@ -18,27 +8,45 @@ end
 table.insert(basicoilprocessingrecipe.results, {type="fluid", name="heavy-oil", amount=30, fluidbox_index = 1})
 table.insert(basicoilprocessingrecipe.results, {type="fluid", name="light-oil", amount=30, fluidbox_index = 2})
 
-local advancedoilprocessingrecipe = data.raw["recipe"]["advanced-oil-processing"]
-for _, result in pairs (advancedoilprocessingrecipe.results) do
-	if result.name == "heavy-oil" then
-		result.amount = 10
-		break
+if settings.startup["pre0-17-60oil-revert-flamethrowerammo"].value then
+	local flamethrowerammorecipe = data.raw["recipe"]["flamethrower-ammo"]
+	for _, ingredient in pairs (flamethrowerammorecipe.ingredients) do
+		if ingredient.name == "crude-oil" then
+			ingredient.name = "light-oil"
+			ingredient.amount = 50
+			break
+		end
+	end
+	table.insert(flamethrowerammorecipe.ingredients, {type="fluid", name="heavy-oil", amount=50})
+end
+
+if settings.startup["pre0-17-60oil-revert-advoilrecipe"].value then
+	local advancedoilprocessingrecipe = data.raw["recipe"]["advanced-oil-processing"]
+	for _, result in pairs (advancedoilprocessingrecipe.results) do
+		if result.name == "heavy-oil" then
+			result.amount = 10
+			break
+		end
 	end
 end
 
-local chemicalsciencepackrecipe = data.raw["recipe"]["chemical-science-pack"]
-for _, ingredient in pairs (chemicalsciencepackrecipe.ingredients) do
-	if ingredient[1] == "sulfur" then
-		ingredient[1] = "solid-fuel"
-		break
+if settings.startup["pre0-17-60oil-revert-chemscipack"].value then
+	local chemicalsciencepackrecipe = data.raw["recipe"]["chemical-science-pack"]
+	for _, ingredient in pairs (chemicalsciencepackrecipe.ingredients) do
+		if ingredient[1] == "sulfur" then
+			ingredient[1] = "solid-fuel"
+			break
+		end
 	end
 end
 
-local rocketfuelrecipe = data.raw["recipe"]["rocket-fuel"]
-rocketfuelrecipe.category = "crafting"
-for p, ingredient in pairs (rocketfuelrecipe.ingredients) do
-	if ingredient.name == "light-oil" then
-		rocketfuelrecipe.ingredients[p] = nil
-		break
+if settings.startup["pre0-17-60oil-revert-rocketfuelrecipe"].value then
+	local rocketfuelrecipe = data.raw["recipe"]["rocket-fuel"]
+	rocketfuelrecipe.category = "crafting"
+	for p, ingredient in pairs (rocketfuelrecipe.ingredients) do
+		if ingredient.name == "light-oil" then
+			rocketfuelrecipe.ingredients[p] = nil
+			break
+		end
 	end
 end
---
+++ settings.lua
@@ -0,0 +1,66 @@
+data:extend({
+	{
+		type = "bool-setting",
+		name = "pre0-17-60oil-revert-chemscipack",
+		description = "pre0-17-60oil-revert-chemscipack",
+		setting_type = "startup",
+		default_value = true,
+		order = "a"
+	},
+	{
+		type = "bool-setting",
+		name = "pre0-17-60oil-revert-lasertechs",
+		description = "pre0-17-60oil-revert-lasertechs",
+		setting_type = "startup",
+		default_value = true,
+		order = "b"
+	},
+	{
+		type = "bool-setting",
+		name = "pre0-17-60oil-revert-rocketrytech",
+		description = "pre0-17-60oil-revert-rocketrytech",
+		setting_type = "startup",
+		default_value = true,
+		order = "c"
+	},
+	{
+		type = "bool-setting",
+		name = "pre0-17-60oil-revert-roboticstechs",
+		description = "pre0-17-60oil-revert-roboticstechs",
+		setting_type = "startup",
+		default_value = true,
+		order = "d"
+	},
+	{
+		type = "bool-setting",
+		name = "pre0-17-60oil-revert-solidfuelrecipes",
+		description = "pre0-17-60oil-revert-solidfuelrecipes",
+		setting_type = "startup",
+		default_value = true,
+		order = "e"
+	},
+	{
+		type = "bool-setting",
+		name = "pre0-17-60oil-revert-rocketfuelrecipe",
+		description = "pre0-17-60oil-revert-rocketfuelrecipe",
+		setting_type = "startup",
+		default_value = true,
+		order = "f"
+	},
+	{
+		type = "bool-setting",
+		name = "pre0-17-60oil-revert-flamethrowerammo",
+		description = "pre0-17-60oil-revert-flamethrowerammo",
+		setting_type = "startup",
+		default_value = true,
+		order = "g"
+	},
+	{
+		type = "bool-setting",
+		name = "pre0-17-60oil-revert-advoilrecipe",
+		description = "pre0-17-60oil-revert-advoilrecipe",
+		setting_type = "startup",
+		default_value = true,
+		order = "h"
+	}
+})
